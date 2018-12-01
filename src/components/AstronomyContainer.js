6a5ffb60986a27fa5441504fa7ae2a6689322027
sleeplesseditor <josh@bryson.me.uk>
2018-12-01 01:56:15 +0000

    Redux implemented

--- src/components/AstronomyContainer.js
+++ src/components/AstronomyContainer.js
@@ -1,41 +1,26 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import fetchData from '../actions/fetch_data';
 import AstronomyCard from './AstronomyCard';
 
 class AstronomyContainer extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-            astronomy: []
-        }
-    }
 
     componentDidMount(){
-        const API_KEY = require('../config/keys').nasaAPI;
-
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-            .then(response => {
-                this.setState({
-                    astronomy: response.data
-                })
-
-                console.log(this.state.astronomy);
-            })
-            .catch(error => {
-                console.log(error, 'Failed to fetch NASA Data')
-            });
+        console.log(this.props.astronomy);
+        this.props.fetchData();
     }
 
     render() {
-        const { astronomy } = this.state;
-
         return (
             <div>
-                <AstronomyCard data={astronomy} />
+                <AstronomyCard data={this.props.astronomy} />
             </div>
         )
     }
 }
 
-export default AstronomyContainer;
+const mapStateToProps = (state) => {
+    return { astronomy: state.astronomy}
+}
+
+export default connect(mapStateToProps, { fetchData })(AstronomyContainer);
---
+++ src/reducers/app_reducer.js
@@ -0,0 +1,9 @@
+export default function (state = {}, action){
+    switch(action.type){
+        case 'FETCH_DATA':
+            const newState = Object.assign({}, state, action.payload.data)
+            return newState;
+        default:
+            return state;
+    }
+}
