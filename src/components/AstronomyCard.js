d62ef8af99e95522afe9ee45d8e1f74f12f387fe
sleeplesseditor <josh@bryson.me.uk>
2018-12-02 01:21:33 +0000

    Social media sharing icons setup

--- src/components/AstronomyCard.js
+++ src/components/AstronomyCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Moment from 'react-moment';
+import { SimpleShareButtons } from "react-simple-share";
 
 const AstronomyCard = (props) => {
     const { title, url, hdurl, explanation, date, copyright } = props.data;
@@ -17,6 +18,14 @@ const AstronomyCard = (props) => {
                     {date}
                 </Moment> 
             </span>
+            <SimpleShareButtons
+                url={url}
+                whitelist={
+                    ["Facebook", "Twitter", "Tumblr", "Pinterest", "Reddit"]
+                }
+                size="40px"
+            />
+
         </div>
     )
 }
