35b67794515e49250b1c1c9c1359cec608baa28c
Josh Bryson <josh@bryson.me.uk>
2019-11-13 20:35:26 +0000

    Initial Jest/Enzyme unit test setup and first test suite implemented

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from "enzyme";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
 import App from './App';
+import Navbar from './components/NavBar';
+import AstronomyList from './components/NavBar';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+describe("App", () => {
+  let component;
+  beforeEach(() => {
+    component = shallow(
+      <Provider store={store}>
+          <App />
+      </Provider>
+  )
+  });
+
+  it("should render without throwing an error", () => {
+      expect(component.exists()).toBe(true);
+  });
+
+  it("should render the Navbar component", () => {
+    const navbarComponent = component.dive().find(Navbar);
+    expect(navbarComponent).toHaveLength(1);
+  });
+
+  it("should render the Astronomy List component", () => {
+    const astronomyListComponent = component.dive().find(AstronomyList);
+    expect(astronomyListComponent).toHaveLength(1);
+  });
 });
